94dd1a705fd26455b10140499272f18ef52af763
chaochungkuo <chao-chung.kuo@rwth-aachen.de>
2023-07-20 18:32:52 +0800

    improve visualization

--- script_render_html.sh
+++ script_render_html.sh
@@ -3,3 +3,4 @@ jupyter nbconvert --to html --output-dir=reports 01_Preprocessing.ipynb
 jupyter nbconvert --to html --output-dir=reports 02_Feature_selection.ipynb
 jupyter nbconvert --to html --output-dir=reports 03_Model_selection.ipynb
 jupyter nbconvert --to html --output-dir=reports 03-01_Logistic_Regression.ipynb
+jupyter nbconvert --to html --output-dir=reports 03-02_Decision_Tree.ipynb
