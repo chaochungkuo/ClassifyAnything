f43a9cb7df91579e91b634a336be738fed1df982
chaochungkuo <chao-chung.kuo@rwth-aachen.de>
2023-07-21 08:08:26 +0800

    add random forest

--- script_render_html.sh
+++ script_render_html.sh
@@ -3,4 +3,5 @@ jupyter nbconvert --to html --output-dir=reports 01_Preprocessing.ipynb
 jupyter nbconvert --to html --output-dir=reports 02_Feature_selection.ipynb
 jupyter nbconvert --to html --output-dir=reports 03_Model_selection.ipynb
 jupyter nbconvert --to html --output-dir=reports 03-01_Logistic_Regression.ipynb
-jupyter nbconvert --to html --output-dir=reports 03-02_Decision_Tree.ipynb
+jupyter nbconvert --to html --output-dir=reports 03-02_Decision_Trees.ipynb
+jupyter nbconvert --to html --output-dir=reports 03-03_Random_Forest.ipynb
