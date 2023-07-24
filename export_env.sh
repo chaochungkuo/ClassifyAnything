71b35fdb9320abb836c6aa587fa862547cf2c101
chaochungkuo <chao-chung.kuo@rwth-aachen.de>
2023-07-24 18:01:37 +0800

    update env

--- export_env.sh
+++ export_env.sh
@@ -4,7 +4,7 @@ conda env export --no-builds | grep -v "prefix" > conda_environment.yml
 conda list --explicit > conda-spec-file.txt
 pip freeze > requirements.txt
 
-# Your can delete your current SplitAnything environment and create a new one by:
+# Your can delete your current ClassifyAnything environment and create a new one by:
 # conda deactivate
-# conda env remove --name SplitAnything
-# conda env create -f conda_environment.yml -n SplitAnything
+# conda env remove --name ClassifyAnything
+# conda env create -f conda_environment.yml -n ClassifyAnything
--- script_rerun_notebooks.sh
+++ script_rerun_notebooks.sh
@@ -1,1 +1,8 @@
 jupyter nbconvert --to notebook --execute 01_Preprocessing.ipynb
+jupyter nbconvert --to notebook --execute 02_Feature_selection.ipynb
+jupyter nbconvert --to notebook --execute 03_Model_selection.ipynb
+jupyter nbconvert --to notebook --execute 03-01_Logistic_Regression.ipynb
+jupyter nbconvert --to notebook --execute 03-02_Decision_Trees.ipynb
+jupyter nbconvert --to notebook --execute 03-03_Random_Forest.ipynb
+jupyter nbconvert --to notebook --execute 03-04_SVM.ipynb
+jupyter nbconvert --to notebook --execute 04_Model_Evaluation.ipynb
